3ab6ba2bbd1ee6bfa6d33f5ea61ad52112a993ae
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-13 19:17:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
 array =  []
 array << string
+reversed = array.collect{ |obj| obj.reverse}
 #seperated  = string.split(/ /)
 #reversed = seperated.collect { |obj| obj.reverse}
 #reversed = seperated.map { |e| e.reverse }
