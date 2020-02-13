099e68ba784f6834337ffc922c606d479c28c6c5
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-13 18:11:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(string)
   counter = 0
-  to_array = []
+  to_array = string.split
   while counter < string.length
     puts to_array << string[counter]
    counter += 1
  end
- 
+
 end
